495b4fdb664e6f592dcada8384394fd8035a1dcd
camilohoyos16 <acamilohoyos@hotmail.com>
2016-10-16 23:37:06 -0500

    Pequeños errorres de Testing

--- Assets/Scripts/General/GoScene.cs
+++ Assets/Scripts/General/GoScene.cs
@@ -7,10 +7,9 @@ public class GoScene : MonoBehaviour {
 	public float timeForChange;
 	public string nameOfScene;
 	private AudioSource audio;
-	private SpawnEnemies[] enemySpawns = new SpawnEnemies[2]; 
 
 	void Start () {		
-		enemySpawns = GameObject.FindObjectsOfType <SpawnEnemies > ();
+
 	}
 	
 	// Update is called once per frame
@@ -26,10 +25,6 @@ public class GoScene : MonoBehaviour {
 
 	public IEnumerator MainMenu()
 	{
-		for (int i = 0; i < 2; i++) 
-		{
-			enemySpawns [i].StopAllCoroutines();
-		}
 		yield return new WaitForSeconds (timeForChange);
 		SceneManager.LoadScene (nameOfScene);
 		StopAllCoroutines ();
